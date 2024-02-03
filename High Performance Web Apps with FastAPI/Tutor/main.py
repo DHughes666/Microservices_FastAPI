38de9c0e1d5386313b7e7a3f3519603fddb0f3f7
DHughes666 <graziemolto6@gmail.com>
2024-02-03 04:02:35 +0100

    So far so interesting...

--- High Performance Web Apps with FastAPI/Tutor/main.py
+++ High Performance Web Apps with FastAPI/Tutor/main.py
@@ -1,5 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
+from typing import Optional
+
 app = FastAPI()
 
 @app.get('/')
@@ -9,6 +11,22 @@ async def index():
 @app.get('/{name}/{id}')
 async def user(name: str, id: int):
     return {"name": name, "id": id}
+
+@app.get('/employee/{name}/{age}')
+async def employee(name: str, age: int):
+    return {"name": name, "age": age}
+
+@app.get('/employee/{name}')
+async def get_employee(name: str, age: int = 20):
+    return {"name": name, "age": age}
+
+@app.get('/employee/{name}/branch/{branch_id}')
+async def get_employee_branch(name: str, brname: str, branch_id:int,
+                              age:Optional[int] = None):
+    employee = {"name": name, 'Branch': brname, 
+                'Branch_id': branch_id, 'Age': age}
+    return employee
+
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
