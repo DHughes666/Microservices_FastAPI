596b06d807a86d6a16aad26f63a8eb9c5d22c165
DHughes666 <graziemolto6@gmail.com>
2024-02-22 20:12:23 +0100

    Project_II_BOOKS ...Validation ...Established

--- F_I/booksProject/books_II.py
+++ F_I/booksProject/books_II.py
@@ -1,11 +1,29 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from uuid import UUID
 from enum import Enum
 from typing import Optional
 
 app = FastAPI()
+
+class Book(BaseModel):
+    id : UUID
+    title : str = Field(min_length=2)
+    author : str = Field(min_length=1, max_length=100)
+    description : Optional[str] = Field(
+        title="Description of the book",
+        max_length=100,
+        min_length=1
+    )
+    rating : int = Field(gt=-1, lt=101)
 
 BOOKS = []
 
 @app.get('/books')
 async def read_all_books():
     return BOOKS
+
+@app.post("/")
+async def create_book(book: Book):
+    BOOKS.append(book)
+    return book
