f1d2f9d501e48873f5ddd2cfedb04834a0dadca0
DHughes666 <graziemolto6@gmail.com>
2024-02-22 19:14:15 +0100

    Books project --CRUD features fully implemented...

--- F_I/booksProject/books.py
+++ F_I/booksProject/books.py
@@ -1,5 +1,7 @@
 # import uvicorn
 from fastapi import FastAPI
+from enum import Enum
+from typing import Optional
 
 app = FastAPI()
 
@@ -12,22 +14,66 @@ BOOKS = {
     'book_6': {'title': 'Contagion', 'author': 'Robin Cook'},
 }
 
+class DirectionName(str, Enum):
+    north = "North"
+    south = "South"
+    east = "East"
+    west = "West"
+
 
 @app.get("/")
 async def first_api():
     return {"message": "Hello, worldyy!"}
 
 @app.get("/books")
-async def read_all_books():
+async def read_all_books(skip_book: Optional[str] = None):
+    if skip_book:
+        new_books = BOOKS.copy()
+        del new_books[skip_book]
+        return new_books
     return BOOKS
+
+@app.get("/{book_name}")
+async def read_book(book_name: str):
+    return BOOKS[book_name]
+
+@app.post("/new_book")
+async def create_book(book_title, book_author: str):
+    current_book_id = 0
+    if len(BOOKS) > 0:
+        for book in BOOKS:
+            x = int(book.split('_')[-1])
+            if x > current_book_id:
+                current_book_id = x
+    BOOKS[f'book_{current_book_id + 1}'] = {'title': book_title, 
+                                            'author': book_author}
+    return BOOKS
+
+@app.put('/{book_name}')
+async def update_book(book_name, book_title, book_author: str):
+    book_info = {'title': book_title, 'author': book_author}
+    BOOKS[book_name] = book_info
+    return BOOKS
+
+@app.delete('/{book_name}')
+async def delete_book(book_name):
+    del BOOKS[book_name]
+    return f'Book_{book_name} has been deleted'
+
+@app.get("/direction/{direction_name}")
+async def read_direction(direction_name: DirectionName):
+    if direction_name == DirectionName.north:
+        return {"Direction": direction_name, "sub": "Up"}
+    if direction_name == DirectionName.south:
+        return {"Direction": direction_name, "sub": "Down"}
+    if direction_name == DirectionName.east:
+        return {"Direction": direction_name, "sub": "Right"}
+    if direction_name == DirectionName.west:
+        return {"Direction": direction_name, "sub": "Left"}
 
 @app.get("/books/mybook")
 async def read_favorite_book():
     return {"book_title": "My Favorite Book"}
-
-@app.get('/books/{book_id}')
-async def read_book(book_id: int):
-    return {"book_title": book_id}
 
 
 
