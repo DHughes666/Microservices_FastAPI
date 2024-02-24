956f37b4bd1e59ecca199967c47ced414120109b
DHughes666 <graziemolto6@gmail.com>
2024-02-24 14:47:11 +0100

    TODOS_PROJECT ... Validating users

--- F_I/todosProject/auth.py
+++ F_I/todosProject/auth.py
@@ -1,8 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from  pydantic import BaseModel
+from sqlalchemy.orm import Session
 from typing import Optional
-import models
 from passlib.context import CryptContext
+
+
+import models
+from database import SessionLocal, engine
+
 
 class CreateUser(BaseModel):
     username: str
@@ -13,13 +19,36 @@ class CreateUser(BaseModel):
 
 bcrypt_context = CryptContext(schemes=["sha256_crypt"], deprecated="auto")
 
+models.Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
 
 def get_password_hash(password):
     return bcrypt_context.hash(password)
 
+def verify_password(password, hashed_password):
+    return bcrypt_context.verify(password, hashed_password)
+
+def authenticate_user(username: str, password: str, db):
+    user = db.query(models.Users).filter(
+        models.Users.username == username).first()
+    
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
 @app.post("/create/user")
-async def create_user(create_user: CreateUser):
+async def create_user(create_user: CreateUser,
+                      db: Session = Depends(get_db)):
     create_user_model = models.Users()
     create_user_model.username = create_user.username
     create_user_model.email = create_user.email
@@ -29,8 +58,19 @@ async def create_user(create_user: CreateUser):
     create_user_model.last_name = create_user.last_name
     create_user_model.is_active = True
 
+    db.add(create_user_model)
+    db.commit()
+
     return {
-        "status": 200,
-        "Details": "User created successfully",
-        "user": create_user_model,
+        'status': 201,
+        'transaction': 'Successful'
     }
+
+@app.post("/token")
+async def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db)):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user: 
+        raise HTTPException(status_code=404, detail="User not found")
+    return "User Validated"
