61a3be7d40a785082019be2b8176d45cff9685bb
DHughes666 <graziemolto6@gmail.com>
2024-03-15 21:46:00 +0100

    Fast_II Project ...first api ...Paused

--- F_II/main.py
+++ F_II/main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 
 app = FastAPI()
 
 @app.get('/')
 def index():
     return 'Hello, world'
+
+@app.get('/blog/{id}', response_description=status.HTTP_404_NOT_FOUND)
+def get_blog(id: int):
+    if id > 5:
+        return {'error': f'Blog {id} not found'}
+    return {'message': f'blog with id {id}'}
