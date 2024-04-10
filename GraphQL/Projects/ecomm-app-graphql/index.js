f14bd33bd82286399bf046dad0a08ad3bf4f43a8
DHughes666 <graziemolto6@gmail.com>
2024-04-10 06:57:53 +0100

    Ecommerce_GraphQL_project...Autorun app

---
+++ GraphQL/Projects/ecomm-app-graphql/index.js
@@ -0,0 +1,21 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+
+const typeDefs = gql`
+    type Query {
+        welcome: String
+    }
+`
+
+const resolvers = {
+    Query: {
+        welcome: () => {
+            return "Welcome to the World of GraphQL"
+        }
+    }
+}
+
+const server = new ApolloServer({typeDefs, resolvers})
+
+server.listen().then(
+    ({ url }) => console.log(`Server listening on ${url}`));
