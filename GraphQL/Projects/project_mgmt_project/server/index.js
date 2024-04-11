fce5bfcb0fa8376f6e5d3a111d1a3ae0ca2e1795
DHughes666 <graziemolto6@gmail.com>
2024-04-11 12:39:45 +0100

    Connecting to mongodb database

--- GraphQL/Projects/project_mgmt_project/server/index.js
+++ GraphQL/Projects/project_mgmt_project/server/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const dbConnect = require('../config/db');
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+dbConnect();
 
 app.use(
     '/graphql',
