8498a07653411401189345b3417a3120864bee6c
DHughes666 <graziemolto6@gmail.com>
2024-05-07 07:44:01 +0100

    Project_management_Project_III ...Initiated

---
+++ GraphQL/Projects/project_mgmt_project_III/schema/schema.js
@@ -0,0 +1,33 @@
+const {projects, clients } = require('../server/dummyData');
+const {
+    GraphQLObjectType, GraphQLID, GraphQLString,
+    GraphQLSchema
+} = require('graphql');
+
+//Client Type
+const ClientType = new GraphQLObjectType({
+    name: 'Client',
+    fields: () => ({
+        id: {type: GraphQLID },
+        name: {type: GraphQLString },
+        email: {type: GraphQLString },
+        phone: {type: GraphQLString },
+    }),
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        client: {
+            type: ClientType,
+            args: {id: {type: GraphQLID} },
+            resolve(parent, args) {
+                return clients.find(
+                    client => client.id === args.id
+                );
+            },
+        },
+    },
+});
+
+module.exports = new GraphQLSchema({ query: RootQuery });
